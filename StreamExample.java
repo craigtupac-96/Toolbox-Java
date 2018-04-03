7cb77ddd9499549c3f6b5b7d073c3936b25bdb92
Craig Lawlor <craigtupac-96@users.noreply.github.com>
2018-04-03 14:39:31 +0100

    Layout change

--- StreamExample.java
+++ StreamExample.java
@@ -11,7 +11,11 @@ import java.util.stream.Stream;
 public class StreamExample {
 
 	public static void main(String[] args) {
-		Arrays.asList("red", "green", "blue").stream().sorted().findFirst().ifPresent(System.out::println);
+		Arrays.asList("red", "green", "blue")
+			.stream()
+			.sorted()
+			.findFirst()
+			.ifPresent(System.out::println);
 		
 		// example of Stream.of with a filter // prints all fruit starting with a
 		Stream.of("apple", "pear", "banana", "cherry", "apricot").filter(fruit -> {
@@ -23,7 +27,8 @@ public class StreamExample {
 		
 		// using a stream and a map operation to create a list of words in caps
 		List<String> collected = Stream.of("Java", " Rocks")
-				.map(String -> String.toUpperCase()).collect(Collectors.toList());
+				.map(String -> String.toUpperCase())
+			.collect(Collectors.toList());
 		System.out.println(collected.toString());
 		
 	}
